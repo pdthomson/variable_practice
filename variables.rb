c6551d3db1bd413d40941ab3ec94c1488bfbf2bd
Paker Thomson <pdthomson1239@gmail.com>
2022-02-07 22:46:23 -0500

    Reassign string

--- variables.rb
+++ variables.rb
@@ -4,8 +4,11 @@ email = "johndoe@yahoo.com"
 age = 45# assigns an intger of 45 to the variable age
 weight = 150
 
-bank_account = 50,000.00# assigns a float to the varriable bank_account
+bank_account = 50000.00# assigns a float to the varriable bank_account
 miles_walked_daily = 4.5
 
 membership_paid = true# assigns a boolean to the variable membership_paid
 netflix_paid = true
+
+puts "#{name} is going to the bank but cant remember his #{email}"
+puts "#{name} likes to walk #{miles_walked_daily} miles every day and think about his age...#{age}"
