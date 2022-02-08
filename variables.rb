b63ed71fbde5d3134f40bae3f2ae2db6f087f149
Paker Thomson <pdthomson1239@gmail.com>
2022-02-07 22:38:34 -0500

    Add booleans

--- variables.rb
+++ variables.rb
@@ -1,8 +1,11 @@
 name = "John Doe"# assigns a variable name as a string to john Doe
 email = "johndoe@yahoo.com"
 
-age = 45
+age = 45# assigns an intger of 45 to the variable age
 weight = 150
 
-bank_account = 50,000.00
+bank_account = 50,000.00# assigns a float to the varriable bank_account
 miles_walked_daily = 4.5
+
+membership_paid = true# assigns a boolean to the variable membership_paid
+netflix_paid = true
